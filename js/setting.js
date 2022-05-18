94f33fd42c45bdca580bcd1755859dd8d33d7fe3
Arnab Saha <72820989+ArnabTo@users.noreply.github.com>
2022-05-18 16:25:39 +0600

    File Upload

---
+++ js/setting.js
@@ -0,0 +1,17 @@
+
+// const fullName = document.getElementById("fullName");
+// const eMail = document.getElementById("eMail");
+// const pNumber = document.getElementById("pNumber");
+// const eMessage = document.getElementById("eMessage");
+// const subButton = document.getElementById("subButton");
+
+// const db = getDatabase();
+// subButton.addEventListener('click',(e)=>{
+//    e.preventDefault();
+//    database.ref('/users/'+ fullName.value ).set({
+// full_name:fullName.value,
+// email:eMail.value,
+// pnumber:pNumber.value,
+// message:eMessage.value
+//    });
+// })
